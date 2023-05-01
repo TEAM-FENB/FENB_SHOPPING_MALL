a019baaed31e8fe53e90c68f66deedda7b15ddf7
Lee Dong Gyu <tenenger7125@gmail.com>
2023-05-01 14:20:26 +0900

    refactor: move pageProductsLoader root path -> main path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,14 +30,14 @@ const router = createHashRouter([
   {
     path: '/',
     element: <Root />,
-    loader: async () => ({
-      products: await productsLoader(),
-      pageProducts: await pageProductsLoader(),
-    }),
+    loader: productsLoader,
     children: [
       {
         path: PATH.MAIN,
-        loader: slidesLoader,
+        loader: async () => ({
+          slides: await slidesLoader(),
+          pageProducts: await pageProductsLoader(),
+        }),
         element: <Main />,
       },
       {
